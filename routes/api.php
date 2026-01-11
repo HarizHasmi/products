5bc0eaeadebe0ef8fb75843cab20855dfdc096f0
HarizHasmi <harizhasmi.fst@gmail.com>
2026-01-11 10:09:07 +0800

    Implement authorization using Spatie permissions

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\ProductsController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,3 +9,7 @@ Route::get('/products/{id}',[ProductsController::class,'show']);
 Route::post('/products',[ProductsController::class,'store']);
 Route::put('/products/{id}',[ProductsController::class, 'update']);
 Route::delete('/products/{id}',[ProductsController::class, 'destroy']);
+Route::post('/auth/register', [AuthController::class, 'register']);
+Route::post('/auth/login',[AuthController::class,'login']);
+Route::post('/auth/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/auth/me',[AuthController::class,'me'])->middleware('auth:sanctum');
